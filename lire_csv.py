15d28ba1f911d16f506b21e9578226d4ea004014
VykeQc <2238330@cegepmontpetit.ca>
2024-03-05 17:02:39 -0500

    files uploaded

---
+++ lire_csv.py
@@ -0,0 +1,17 @@
+#! /bin/python
+import csv
+import os
+os.chdir(os.path.dirname(__file__))
+
+# Q3
+# Ouvrez le ficher en lecture
+# Utilisez le module csv pour lire le contenu du fichier
+# Sautez la première ligne contenant les en-têtes
+# Ajoutez chaque ligne à la variable list_client
+# Finalement, imprimer la variable list_client
+
+nom_fichier = "fichier_a_lire.csv"
+list_client = []
+
+
+
---
+++ traitement_infos_clients.py
@@ -0,0 +1,31 @@
+import nouvelles_donnees as nd
+
+
+# cette variable consiste en une liste de 500 dictionnaires, chacun correspondant à 1 client d'une agence d'aviation.
+# ( voir le fichier nouvelles_donnees.py pour regarder à quoi ces données ressemblent )
+donnees_voyage = nd.donnees_voyage
+
+
+
+# Regarde cette liste de dictionnaires
+# On veut obtenir des données sur les voyages effectués par les clients
+# Obtenez   - Le nombre de personnes ayant fait un voyage en "Chine"
+#           - Le nombre de personnes ayant fait un voyage au "Japon"
+#           - Le nombre total de personnes ayant fait un voyage en "Chine" et/ou au "Japon"...
+#                       - ... et le pourcentage des tous les voyageurs ayant visité la Chine ou le Japon
+# Imprimez les résultats obtenus
+cpt_chine = 0
+cpt_japon = 0
+cpt_asie = 0
+
+
+
+
+
+
+
+
+# Question Bonus : 
+# Obtenez et imprimez une liste de tous les pays visités par des voyageurs.
+# Cette liste ne devrait pas contenir de duplicatas ( chaque pays apparait une seule fois )
+
